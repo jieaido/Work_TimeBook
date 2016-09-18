982913322124cd2541914bfc227e42e2168689b6
jieaido <jieaido@163.com>
2016-09-18 21:48:27 +0800

    配置了一个折叠菜单。menubar

---
+++ Work_TimeBook/Site/Controllers/MenuController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Site.Models;
+
+namespace Site.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult BarMenu()
+        {
+            var m1=new Menubar()
+            {
+               
+           };
+           var barmenus=new List<Menubar>()
+           {
+               new Menubar()
+               {
+                    MenuId = 1,
+                MenusNam = "测1"
+               },new Menubar()
+               {
+                   MenuId = 2,
+                MenusNam = "测2",ParentId = 1
+                },new Menubar()
+               {
+                   MenuId = 3,
+                MenusNam = "测2"
+                },new Menubar()
+               {
+                   MenuId = 4,
+                MenusNam = "测2",ParentId = 3
+                },new Menubar()
+               {
+                   MenuId = 5,
+                MenusNam = "测2",
+                },new Menubar()
+               {
+                   MenuId = 6,
+                MenusNam = "测2",ParentId = 3
+                },new Menubar()
+               {
+                   MenuId = 7,
+                MenusNam = "测2",ParentId = 3
+                },new Menubar()
+               {
+                   MenuId = 8,
+                MenusNam = "测2",ParentId = 5
+                },new Menubar()
+               {
+                   MenuId = 9,
+                MenusNam = "测2",ParentId = 5
+                },
+           };
+
+            return PartialView(barmenus)   ;
+        }
+    }
+}
---
+++ Work_TimeBook/Site/Models/Menubar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site.Models
+{
+    public class Menubar
+    {
+        public int MenuId { get; set; } 
+        public string MenusNam { get; set; }
+        public int ParentId { get; set; }
+        
+    }
+}
