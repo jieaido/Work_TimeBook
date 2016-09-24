b1023d57137cb55d8ab79a0e9df5e697db69b59f
jieai)t460p <jieai@163.com>
2016-09-24 20:12:19 +0800

    增加function表

--- Work_TimeBook/Entity/EFDbContext.cs
+++ Work_TimeBook/Entity/EFDbContext.cs
@@ -28,9 +28,5 @@ namespace Entity
         public virtual DbSet<Menu> Menus { get; set; }
     }
 
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
+   
 }
